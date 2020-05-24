30daa8a0f9457ef66668e77547378e19db1f0595
Rabbit-inmyheart <62099472+Rabbit-inmyheart@users.noreply.github.com>
2020-05-24 18:16:36 +0800

    Create HearthstoneIDcard.py

---
+++ HearthstoneIDcard.py
@@ -0,0 +1,25 @@
+import requests
+from lxml import etree
+import pickle
+dict_1 = {}
+num = 0
+filename = 'd\\hearthstone.pkl'#存储路径，自己填
+while(num<=11000):
+    url = 'https://hearthstone.huijiwiki.com/index.php?title=%E5%B1%9E%E6%80%A7:'+'CardId&limit=500&offset=%d&value=&from=&until='%num
+    r = requests.get(url)
+    r_xpath = etree.HTML(r.text)
+    r_x = r_xpath.xpath('//tr/td[1]/a/text()')
+    r_x1 = r_xpath.xpath('//tr/td[2]/text()')
+    for i in range(0,len(r_x)-1):
+        dict_1[r_x[i]]= r_x1[i]
+        print(dict_1[r_x[i]])
+    num +=500
+f = open('filename','wb')
+pickle.dump(dict_1,f)
+f.close()
+'''
+#读取字典
+f = open('filename','rb')
+r = pickle.load(f)
+f.close()
+'''
